6bc28f2836a7e695e4efd5458f5c19e53bdb1686
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-07-20 20:22:44 +0530

    new msg

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let output = document.getElementById("output");
 let startBtn = document.getElementById("startBtn");
 let stopBtn = document.getElementById("stopBtn");
 let languageSelect = document.getElementById("language");
+
+
+if (!("webkitSpeechRecognition" in window)) {
+  alert("Speech recognition not supported in this browser. Try Chrome.");
